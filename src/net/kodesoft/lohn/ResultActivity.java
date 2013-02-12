916717893b2fd41fe77046ad0919fa3318c787c8
Manuel Gomez <manuel.gomez@advanced.info>
2013-02-12 18:25:15 +0100

    initial

---
+++ src/net/kodesoft/lohn/ResultActivity.java
@@ -0,0 +1,11 @@
+package net.kodesoft.lohn;
+
+import android.app.Activity;
+import android.os.Bundle;
+ 
+public class ResultActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_layout);
+    }
+}
